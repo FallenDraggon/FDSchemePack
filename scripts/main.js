9572c796c2c08a89031f0b6ff096a6c5f5712429
FallenDragon <fallenhereticdragon@gmail.com>
2024-01-12 20:14:26 +0300

    v1

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const mod = Vars.mods.locateMod("FDSchemePack");
+mod.meta.displayName = "FD Scheme Pack";
+mod.meta.description = "Очередной никому ненужный схемпак.";
+mod.meta.author = "[#fa]Fallen[#0]Dragon";
